291be6adddd408301d19e793d4593dbdd0f182f9
Ruslan Nabiullin <whitebarsfan@gmail.com>
2022-08-14 21:29:29 +0300

    Initial Commit

---
+++ LoginApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Mac on 14/08/2022.
+//  Copyright © 2022 Russel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
